cfa8cf964580a886eaf24b5c3aae60d34ab0f8a2
MiseryForMe <1535640262@qq.com>
2019-07-06 20:05:09 +0800

    delete the personnal information

--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo apt-key add /unsullied/sharefs/zhuxingyu/isilon-share/Nvim/ppa/neovim/gpg.key
-echo "deb file:///unsullied/sharefs/zhuxingyu/isilon-share/Nvim/ppa/neovim xenial main " | sudo tee /etc/apt/sources.list.d/neovim.list
 sudo apt update
 sudo apt install -y neovim
 sudo apt-get install -y zsh
 sudo apt-get install -y tmux
+cp -rf ./.zsh ~/
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.config/nvim
 cp ./init.vim ~/.config/nvim
