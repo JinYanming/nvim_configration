8948874c3dc9a3acf2aa83f029f2deec65602d31
MiseryForMe <1535640262@qq.com>
2019-07-07 05:51:48 +0800

    modify
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify1
    
    modify1
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    modify
    
    change jinyanming to ~ at sh file
    
    add the draw frame module
    
    fix somes bugs
    
    removed the auto-pairs
    
    add the auto-pairs
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some not reasonable ways about installations
    
    fixed some bugs between YCM and NCM2 settings
    
    fix some bugs
    
    fix some bugs
    
    fix some bugs
    
    注释了sudo apt-get install -y python-software-properties
    
    YCM still
    
    fzf nvim
    
    fzf nvim

--- .zshrc
+++ .zshrc
@@ -98,5 +98,4 @@ git
 zsh-autosuggestions 
 zsh-syntax-highlighting
 )
-[ -f ~/.fzf.zsh ] && source ~/.zsh/.fzf.zsh
 source $ZSH/oh-my-zsh.sh
--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -12,29 +12,41 @@ if [ -d ~/.zsh ];then
 sudo rm -rf ~/.zsh
 mkdir ~/.zsh
 fi
+echo "${BLUE} neovim zsh tmux ctags installation start"
+
+sudo apt-get update
 sudo apt-get install -y software-properties-common
-sudo apt-get install -y python-software-properties 
+#sudo apt-get install -y python-software-properties 
 sudo apt-add-repository -y ppa:neovim-ppa/stable
-sudo apt update
+sudo apt-get update
 sudo apt-get install -y neovim
 sudo apt-get install -y zsh
 sudo apt-get install -y tmux
 sudo apt-get install -y ctags
+sudo apt-get install -y build-essential cmake python3-dev
+sudo apt-get install -y git
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip 
+sudo pip install pynvim jedi
+sudo pip3 install pynvim jedi
+echo "${GREEN} neovim zsh tmux ctags installation is completed${RESET}"
 echo "${BLUE}install oh my zsh${RESET}"
 sudo rm -rf ~/.oh-my-zsh
-echo y|sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo y|sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "${GREEN} oh my zsh installation is completed${RESET}"
 echo "${BLUE}make zsh default${RESET}" 
 sudo usermod -s /bin/zsh root
 sudo usermod -s /bin/zsh $USER
 
-echo "${BLUE}install vim-plug${RESET}"
+echo "${GREEN}  zsh  is set as default${RESET}"
+echo "${BLUE}install vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim
-sudo cp ./init.vim ~/.config/nvim
-sudo chmod 755 ~/.config/nvim/init.vim
+cat ./init.vim > ~/.config/nvim/init.vim
 
+echo "${GREEN} vim-plug installation is completed${RESET}"
 rm -rf ~/.local/share/nvim
 cp -rf ./nvim ~/.local/share
 if [ -f ~/.zshrc ];then
@@ -43,27 +55,36 @@ fi
 if [ -f ~/.tmux.conf ];then
 sudo rm ~/.tmux.conf
 fi
-sudo cp ./.zshrc ~/
-sudo chmod 755 ~/.zshrc
-sudo cp ./.tmux.conf ~/
-sudo chmod 755 ~/.tmux.conf
+cat ./.zshrc > ~/.zshrc
+cat ./.tmux.conf > ~/.tmux.conf
+
+echo "${BLUE}installing YCM plugins"
+cat ./global_extra_conf.py > ~/global_extra_conf.py
+echo "${GREEN}YCM plugins installation is completed${RESET}"
+echo "${BLUE}installing tmux plugins"
+if [ -d ~/.tmux/plugins/tpm ];then
+sudo rm -r ~/.tmux/plugins/tpm
+fi
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "${GREEN} tmux plugins installation is completed${RESET}"
 if [ ! -d ~/.vim/tags ];then
 mkdir -p ~/.vim/tags/
 fi
-echo "${BLUE}install fzf${RESET}"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.zsh/.fzf
-~/.zsh/.fzf/install
-echo "${YELLOW} fzf installation is completed${RESET}"
-echo "${BLUE}install zsh autosuggestions${RESET}"
+echo "${BLUE}install fzf"
+rm -rf ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+echo n|sudo ~/.fzf/install
+echo "${GREEN} fzf installation is completed${RESET}"
+echo "${BLUE}install zsh autosuggestions"
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "${YELLOW} zsh autosuggestions installation is completed${RESET}"
-echo "${BLUE}install zsh-syntax-highlighting${RESET}"
+echo "${GREEN} zsh autosuggestions installation is completed${RESET}"
+echo "${BLUE}install zsh-syntax-highlighting"
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "${YELLOW} zsh-syntax-highlighting installation is completed${RESET}"
+echo "${GREEN} zsh-syntax-highlighting installation is completed${RESET}"
 
-echo "${RED}install over${RESET}"
-source ~/.zshrc
-ctags -R -f ~/.vim/tags/python3.ctags /usr/lib/python3.5/
-ctags -R -f ~/.vim/tags/python3-dp.ctags /usr/local/lib/python3.5/dist-packages
-ctags -R -f ~/.vim/tags/python3-sp.ctags /home/jinyanming/.local/lib/python3.5/site-packages
+echo "${RED}now generate ctags"
+sudo ctags -R -f ~/.vim/tags/python3.ctags /usr/lib/python3.5/
+sudo ctags -R -f ~/.vim/tags/python3-dp.ctags /usr/local/lib/python3.5/dist-packages
+sudo ctags -R -f ~/.vim/tags/python3-sp.ctags ~/.local/lib/python3.5/site-packages
+echo "${GREEN}ctags generating is over!${RESET}"
 echo "${GREEN}All steps is done!${RESET}"
