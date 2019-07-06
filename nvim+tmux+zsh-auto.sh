d0459329a96b1ba5da0683f4015f391aae4ae341
MiseryForMe <1535640262@qq.com>
2019-07-07 02:16:17 +0800

    delete the personnal information

--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -12,17 +12,17 @@ sudo apt update
 sudo apt-get install -y neovim
 sudo apt-get install -y zsh
 sudo apt-get install -y tmux
-'''install oh my zsh'''
+echo "${BLUE}install oh my zsh"
 sudo rm -rf ~/.oh-my-zsh
 echo y|sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-'''
-make zsh default 
-'''
+
+echo "${BLUE}make zsh default" 
+
 chsh -s /bin/zsh root
 chsh -s /bin/zsh
 
 
-'''install vim-plug'''
+echo "${BLUE}install vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim
@@ -30,7 +30,7 @@ cp ./init.vim ~/.config/nvim
 
 rm -rf ~/.local/share/nvim
 cp -rf ./nvim ~/.local/share
-echo "The program will automaticlly make the ctags needed/n"
+echo "${YELLOW}The program will automaticlly make the ctags needed/n"
 sudo apt-get install ctags
 if [-f "~/.zshrc"];then
 rm ~/.zshrc
@@ -43,16 +43,17 @@ sudo cp ./.tmux.conf ~/
 if [ ! -d "~/.vim/tags" ];then
 mkdir -p ~/.vim/tags/
 fi
-'''install fzf'''
+ehco "${BLUE}install fzf"
 sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf
 ~/.zsh/.fzf/install
-'''install zsh autosuggestions'''
+echo "${BLUE}install zsh autosuggestions"
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-'''install zsh-syntax-highlighting'''
+echo "${BLUE}install zsh-syntax-highlighting"
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-'''install over'''
+echo "${RED}install over"
 source ~/.zshrc
 ctags -R -f ~/.vim/tags/python3.ctags /usr/lib/python3.5/
 ctags -R -f ~/.vim/tags/python3-dp.ctags /usr/local/lib/python3.5/dist-packages
 ctags -R -f ~/.vim/tags/python3-sp.ctags /home/jinyanming/.local/lib/python3.5/site-packages
+echo "${GREEN}All steps is done!"
