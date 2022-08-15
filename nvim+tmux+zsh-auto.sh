1747e80ad5f0c0ef8754a8cf8c67e2a7406b9590
JinYanming <1535640262@qq.com>
2022-08-15 17:06:05 +0800

    nits

--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -16,7 +16,7 @@ echo "${BLUE} neovim zsh tmux ctags installation start"
 
 sudo apt-get update
 sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 #sudo apt-get install -y python-software-properties 
 sudo apt-add-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
