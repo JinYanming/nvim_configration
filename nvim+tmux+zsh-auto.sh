f67185dc327c0611802a3379be1b832bf2172336
JinYanming <1535640262@qq.com>
2023-06-13 17:05:43 +0800

    nits: make nvim the defult editor of git

--- nvim+tmux+zsh-auto.sh
+++ nvim+tmux+zsh-auto.sh
@@ -103,6 +103,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+git config --global core.editor "nvim"
 echo "${GREEN} lazy-git is completed${RESET}"
 
 echo "${GREEN}All steps is done!${RESET}"
